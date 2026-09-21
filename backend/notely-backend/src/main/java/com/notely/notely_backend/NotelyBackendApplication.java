42b7173c456f8d4f1b7c17eac70319a3aa325124
Ani111112 <aniruddhamukherjee232@gmail.com>
2026-09-21 13:10:57 +0530

    Backend Setup

---
+++ backend/notely-backend/src/main/java/com/notely/notely_backend/NotelyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.notely.notely_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotelyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotelyBackendApplication.class, args);
+	}
+
+}
